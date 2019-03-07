ff9c9f482dbac1db7781b2256894b2792ebf0807
Andrew Sosa <andrew@andrewsosa.com>
2019-03-07 14:59:51 -0500

    Add all initial files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: [
+        'airbnb-base',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
+    parser: 'babel-eslint',
+    plugins: ['prettier'],
+    env: {
+        browser: true,
+        jest: true,
+        es6: true,
+    },
+    rules: {
+        'no-console': 'off',
+        'prettier/prettier': 'error',
+        'react/prop-types': 'off',
+    },
+};
