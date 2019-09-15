a1012b7963fcc1d12068e61e2aea1a08693d49eb
Andrew Sosa <andrew@andrewsosa.com>
2019-09-15 00:18:56 -0400

    dumping in old changes

--- web/App.js
+++ web/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import Section from "./layout/Section";
+import TicketTable from "./components/TicketTable";
 
 const App = () => {
-  return <Section>Hello, world!</Section>;
+  return (
+    <Section>
+      <TicketTable />
+    </Section>
+  );
 };
 
 export default App;
--- web/layout/Container.js
+++ web/layout/Container.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "../../node_modules/spectre.css/dist/spectre.css";
 
 const Container = ({ children, style }) => {
   return (
-    <div className="container" style={style}>
+    <div className={styles.container} style={style}>
       {children}
     </div>
   );
