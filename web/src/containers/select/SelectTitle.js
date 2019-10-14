ea5406d8a3cd72fa218d79689837b0ed5e3db171
Andrew Sosa <andrew@andrewsosa.com>
2019-10-13 21:15:01 -0400

    Title has different default text

--- web/src/containers/select/SelectTitle.js
+++ web/src/containers/select/SelectTitle.js
@@ -6,25 +6,27 @@ export default function CompareTitle() {
 
   return (
     <h1 className="f2 fw2">
-      {packages.length === 0
-        ? "Compare freshness of packages"
-        : [...packages]
-            .join("-vs-")
-            .split("-")
-            .map((item, pos) => {
-              return item === "vs" ? (
-                <span key={pos}>{" vs "}</span>
-              ) : (
-                <span key={pos} className="fw4">
-                  <a
-                    className="black no-underline"
-                    href={`https://github.com/${item}`}
-                  >
-                    {item}
-                  </a>
-                </span>
-              );
-            })}
+      {packages.length === 0 ? (
+        <span className="fw4">Compare Github interactions over time</span>
+      ) : (
+        [...packages]
+          .join("-vs-")
+          .split("-")
+          .map((item, pos) => {
+            return item === "vs" ? (
+              <span key={pos}>{" vs "}</span>
+            ) : (
+              <span key={pos} className="fw4">
+                <a
+                  className="black no-underline"
+                  href={`https://github.com/${item}`}
+                >
+                  {item}
+                </a>
+              </span>
+            );
+          })
+      )}
     </h1>
   );
 }
