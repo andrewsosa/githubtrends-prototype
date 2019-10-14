aee042ce0c049a62021dffea350e2098ca107542
Andrew Sosa <andrew@andrewsosa.com>
2019-10-13 21:14:50 -0400

    Header is now a link

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <h1 className="fw2">githubtrends</h1>
+      <Link to="/" className="no-underline">
+        <h1 className="fw2 black">githubtrends</h1>
+      </Link>
     </div>
   );
 }
